0703fc9707245a459cfcd4b77c673a9156fa969a
Eric Walti <ewalti@gmail.com>
2015-04-20 14:08:30 -0700

    add template

--- addon/components/addon.js
+++ addon/components/addon.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
+import layout from '../templates/addon';
 
 export default Ember.Component.extend({
 
-  tagName: 'addon-skeleton-component'
+  tagName: 'addon-skeleton-component',
+
+  layout: layout
 
 });
